3afcc7b7750c7044d50d048ab31e47550c022e55
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-07 15:02:46 -0500

    add route to get campgrounds

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const Campground = require('./models/Campground');
 const connectDB = require('./config/connectDB');
 const app = express();
 
@@ -10,6 +11,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find();
+
+  res.render('campgrounds/index', {
+    campgrounds,
+  });
 });
 
 app.listen(2001, () => console.log('Server is live at: http://localhost:2001'));
