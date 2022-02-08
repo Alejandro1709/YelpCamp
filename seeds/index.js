c5361c84cd25b622ea5480ac4e81c39cf793168f
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-07 19:29:38 -0500

    change some values to seeder

--- seeds/index.js
+++ seeds/index.js
@@ -13,9 +13,14 @@ const seedDB = async () => {
 
   for (let i = 1; i <= 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui unde voluptatem vel corrupti at magnam, labore sint hic aliquam ullam recusandae numquam ipsum magni? Dolorem non ea ducimus! Blanditiis, corporis.',
+      price,
     });
     await camp.save();
   }
