c35649e7bebcb7760dcaa018e48efe1ea1f4e7df
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-11 15:05:28 -0500

    change mongo uri to match node env

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+    let db =
+      process.env.NODE_ENV === 'development'
+        ? process.env.MONGO_URI_DEV
+        : process.env.MONGO_URI_PROD;
+
+    await mongoose.connect(db);
 
     console.log('Connected to DB');
   } catch (error) {
